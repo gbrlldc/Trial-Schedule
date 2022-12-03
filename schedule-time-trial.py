08e6615ba8db971eb8d3729f8d3df3a38cfa7ddd
Gab DC <gabrielle.delacruz@obf.ateneo.edu>
2022-12-03 22:46:33 +0800

    first commit

---
+++ schedule-time-trial.py
@@ -0,0 +1,28 @@
+import schedule
+import time
+
+#set up
+def job1():
+    print("Hello world in minutes!")
+
+def job2():
+    print("Hello world in hours!")
+
+#Ask if user would like to schedule every() minute or every hourS
+input1 = input ('minutes or hours? ')
+
+#If minutes; 
+if input1 == 'minutes':
+    Minutes = int(input("Number in minutes:"))
+    schedule.every(Minutes).minutes.do(job1)
+
+
+elif input1 == 'hours':
+    Hours = int(input("Number in hours:"))
+    schedule.every(Hours).hours.do(job2)
+
+while True:
+    # Checks whether a scheduled tas
+    # is pending to run or not
+    schedule.run_pending()
+    time.sleep(1)
